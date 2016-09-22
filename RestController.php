e7de8abd6b0d88a1e47e7215dd78a3cb82ea6a0b
Seb <meishes@hochschule-trier.de>
2016-09-22 19:59:15 +0200

    RestController extended

--- RestController.php
+++ RestController.php
@@ -5,22 +5,19 @@ $postdata = file_get_contents("php://input");
 if(isset($postdata)){
 	$request = json_decode($postdata);
 	
+	// Client tries to trigger and openLigaDB server reload
 	// To be changed to the correct open liga db fetch
 	if(isset($request->serverFetchOpenLigaDB)){
-		$handle = fopen("http://www.openligadb.de/api/getmatchdata/em2016/2016/1", "r");
-		$line = fgets($handle);
-		$matchesArray = json_decode($line, true);
-		var_dump($matchesArray);
-		$sqlAPI = new SQLAPI();
-		
-	}
+		echo('success');
+	};
 	
-	// Client sends {tablename: someTablename, version: someTimestamp}
+	// Client sends {tablename: someTablename, version: someTimestamp, benutzer_id: IDOfLoggedInBenutzer}
 	// All table entries in the server table with server.version > client.version are unknown for the client
 	// Server will return all data of that table where server.version > client.version to bring the client to the up-to-date table content
 	if(isset($request->tablename) && isset($request->version)){
 		$sqlAPI = new SQLAPI();
 		$data = $sqlAPI->getTableContent($request->tablename, $request->version);
+		
 		// If the Benutzer table was requested, cut off the email and passwort
 		if($request->tablename == 'Benutzer'){
 			$dataAsArray = json_decode($data, true);
@@ -30,6 +27,36 @@ if(isset($postdata)){
 			}
 			$data = json_encode($dataAsArray);
 		};
+		
+		// If the Tipp table was requested, cut off the Tipps that belong to the user (he already has them, we dont want to override them)
+		if($request->tablename == 'Tipp' && isset($request->benutzer_id)){
+			$dataAsArray = json_decode($data, true);
+			$cuttedDataAsArray = array();
+			for($i=0;$i<count($dataAsArray);$i++){
+				if($dataAsArray[$i]["benutzer_fid"] != $request->benutzer_id){
+					$dataAsArray[$i]["status"] = 'committed'; // Client has this row to know what data is already present on the server (offline created tipps on the client have status = not_committed)
+					$cuttedDataAsArray[] = $dataAsArray[$i];
+				}
+			}
+			$data = json_encode($cuttedDataAsArray);
+		};
+		
+		// If the benutzer_spielt_tipprunde table was requested, calculate the punkte for all users before data is returned to the client
+		if($request->tablename == 'benutzer_spielt_tipprunde'){
+			$data = $sqlAPI->getTableContent($request->tablename, 0);
+			$dataAsArray = json_decode($data, true);
+			
+			
+			$cuttedDataAsArray = array();
+			for($i=0;$i<count($dataAsArray);$i++){
+				if($dataAsArray[$i]["benutzer_fid"] != $request->benutzer_id){
+					$dataAsArray[$i]["status"] = 'committed'; // Client has this row to know what data is already present on the server (offline created tipps on the client have status = not_committed)
+					$cuttedDataAsArray[] = $dataAsArray[$i];
+				}
+			}
+			$data = json_encode($cuttedDataAsArray);
+		};
+		
 		echo($data);
 	};
 	
