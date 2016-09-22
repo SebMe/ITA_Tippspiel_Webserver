6a8b9e1c591235c8aac86470886bf480c075d016
Yves Schmit <Yves@yvess-mbp.localdomain>
2016-09-22 23:31:59 +0200

    Tabelle fuellen 2

--- loadgames.php
+++ loadgames.php
@@ -2,7 +2,8 @@
 require_once("SQLAPI.php");
 
 // Connect to server
-$mysqli = new mysqli('127.0.0.1', 'root', 'password');
+//$mysqli = new mysqli('127.0.0.1', 'root', 'password');
+$mysqli = new mysqli('127.0.0.1', 'ita', 'itaem2016');
 if($mysqli->connect_error){
 	var_dump($mysqli->connect_error);
 }
@@ -23,15 +24,20 @@ $line = fgets(fopen("http://www.openligadb.de/api/getmatchdata/em2016/2016/5", "
 $matchesArray5 = json_decode($line, true);
 $line = fgets(fopen("http://www.openligadb.de/api/getmatchdata/em2016/2016/6", "r"));
 $matchesArray6 = json_decode($line, true);
-
+$line = fgets(fopen("http://www.openligadb.de/api/getmatchdata/em2016/2016/7", "r"));
+$matchesArray7 = json_decode($line, true);
+$line = fgets(fopen("http://www.openligadb.de/api/getmatchdata/em2016/2016/8", "r"));
+$matchesArray8 = json_decode($line, true);
+$line = fgets(fopen("http://www.openligadb.de/api/getmatchdata/em2016/2016/9", "r"));
+$matchesArray9 = json_decode($line, true);
+$line = fgets(fopen("http://www.openligadb.de/api/getmatchdata/em2016/2016/10", "r"));
+$matchesArray10 = json_decode($line, true);
 
 
 // Fülle Tabelle "Zeitzone" mit JSON Daten (nur ein Datensatz notwendig, da Zeitzone überall gleich)
 foreach($matchesArray1 as $i){}
 $sql = "INSERT INTO Zeitzone(zeitzone_name) VALUES ('$i[TimeZoneID]')";
 $mysqli->query($sql);
-
-
 
 
 // Fülle Tabelle "Gruppe" (Von Hand)
@@ -149,11 +155,52 @@ $sql = "INSERT INTO Europameisterschaft(europameisterschaft_jahr, europameisters
 $mysqli->query($sql);
 
 
+function getMannschaftID($name)
+{
+	$select = "SELECT mannschaft_id FROM Mannschaft WHERE mannschaft_name = '$name'";
+	try{
+		$sqlAPI = new SQLAPI();
+		$result = $sqlAPI->executeSQL($select);	
+		$row = $result->fetch_array(MYSQLI_NUM);
+	} catch(Exception $e){
+		echo($e->getMessage());
+	}
+	
+	return $row[0];
+}
+
+
+// Fülle Tabelle "Begegnung" 
+$sql = "ALTER TABLE Begegnung ADD UNIQUE unique_index(begegnung_spieltermin, heimmannschaft_fid, auswaertsmannschaft_fid)";
+try{
+	$sqlAPI = new SQLAPI();
+	$sqlAPI->executeSQL($sql);	
+} catch(Exception $e){
+	echo($e->getMessage());
+}
+
 function fillBegegnung($matchesArray, $gruppeID){
-	// Fülle Tabelle "Begegnung" (Fehlen der FIDs)
+
 	foreach($matchesArray as $i){
-			echo($i[MatchResults][1][PointsTeam2]);
-		$sql = "INSERT INTO Begegnung(begegnung_spieltermin, begegnung_tore_heimmannschaft, begegnung_tore_auswaertsmannschaft, zeitzone_fid, gruppe_fid, europameisterschaft_fid) VALUES ('$i[MatchDateTime]', '$i[MatchResults][1][PointsTeam1]', '$i[MatchResults][1][PointsTeam2]', 1, '$gruppeID', 1)";
+		$tore1 = $i[MatchResults][1][PointsTeam1];
+		$tore2 = $i[MatchResults][1][PointsTeam2];
+		
+		if ($i[MatchResults][2] != null){
+			$tore1 = $i[MatchResults][2][PointsTeam1];
+			$tore2 = $i[MatchResults][2][PointsTeam2];
+		}
+		
+		if ($i[MatchResults][3] != null){
+			$tore1 = $i[MatchResults][3][PointsTeam1];
+			$tore2 = $i[MatchResults][3][PointsTeam2];
+		}
+		
+		$team1 = $i[Team1][TeamName];
+		$team2 = $i[Team2][TeamName];
+		$teamID1 = getMannschaftID($team1);
+		$teamID2 = getMannschaftID($team2);
+				
+		$sql = "INSERT INTO Begegnung(begegnung_spieltermin, begegnung_tore_heimmannschaft, begegnung_tore_auswaertsmannschaft, zeitzone_fid, gruppe_fid, europameisterschaft_fid, heimmannschaft_fid, auswaertsmannschaft_fid) VALUES ('$i[MatchDateTime]', '$tore1', '$tore2', 1, '$gruppeID', 1, '$teamID1', '$teamID2') ON DUPLICATE KEY UPDATE begegnung_spieltermin = '$i[MatchDateTime]', begegnung_tore_heimmannschaft = '$tore1', begegnung_tore_auswaertsmannschaft = '$tore2', heimmannschaft_fid = '$teamID1', auswaertsmannschaft_fid = '$teamID2'";
 		try{
 			$sqlAPI = new SQLAPI();
 			$sqlAPI->executeSQL($sql);	
@@ -163,11 +210,17 @@ function fillBegegnung($matchesArray, $gruppeID){
 	};
 };
 
+
 fillBegegnung($matchesArray1, 1);
 fillBegegnung($matchesArray2, 2);
 fillBegegnung($matchesArray3, 3);
 fillBegegnung($matchesArray4, 4);
 fillBegegnung($matchesArray5, 5);
 fillBegegnung($matchesArray6, 6);
+fillBegegnung($matchesArray7, 7);
+fillBegegnung($matchesArray8, 8);
+fillBegegnung($matchesArray9, 9);
+fillBegegnung($matchesArray10, 10);
+
  
 ?>
